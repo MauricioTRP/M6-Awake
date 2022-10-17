1ef9cb0832e72822b0fc8022809c79e285b98ee3
Mauricio Fuentes <mauriciofb@gmail.com>
2022-10-17 01:08:40 -0300

    ejercicio grupal 3

--- identificador_3b/init.rb
+++ identificador_3b/init.rb
@@ -1,18 +1,17 @@
-def new_product
-  puts "#{'-'*25}Agregue un nuevo producto#{'-'*25}"
+@attributes = %w[clave nombre unidad precio promocion categoria]
+@usd = 0
+@clp = 0
 
-  attributes = %w[clave nombre unidad precio promocion categoria]
-
-  product = fill_product_with(attributes)
-  attributes.each_with_index do |value, index|
-    puts "#{value}: #{product[index]}"
-  end
+# Creates a new product
+def new_product(attributes, iter)
+  fill_product_with(attributes, iter)
 end
 
-def fill_product_with(attributes)
+# Logic of the product creation
+def fill_product_with(attributes, iter)
   array = []
   attributes.each do |attribute|
-    puts "Ingrese #{attribute}"
+    puts "Ingrese #{attribute} del producto #{iter}"
     pass_test = attribute == 'precio' || attribute == 'promocion'
     if pass_test
       array << ask_currency if attribute == 'precio'
@@ -24,23 +23,62 @@ def fill_product_with(attributes)
   array
 end
 
+# Defines the currency of the product
 def ask_currency
   price = gets.chomp.to_i
   if price < 1000
     puts '¿El precio está en dólares? Si (s) - No (n)'
     answer = gets.chomp
-    return "#{price} USD" if answer == 's'
-    return "#{price} CLP" if answer == 'n'
+    if answer == 's'
+      @usd += price
+      return "#{price} USD"
+    elsif answer = 'n'
+      @clp += price
+      return "#{price} CLP"
+    end
   else
+    @clp += price
     return "#{price} CLP"
   end
 end
 
-def start
-  puts "#{'-'*74}\n|#{' '*15}Bienvenido al identificador de precios 3b's#{' '*15}|\n#{'-'*74}"
-  puts "\n"*3
+# Prints product in console
+def print_product(product, iter, attributes)
+  puts "#{'-' * 10} La información del producto #{iter} es #{'-' * 15}"
 
-  new_product
+  attributes.each_with_index do |value, index|
+    puts "#{value}: #{product[index]}"
+  end
 end
 
-start
+# totalizes currencies
+def totals
+  currency = %w[USD CLP]
+  puts "#{'-'*10} Las sumas totales son: #{'-'*10}\n"
+  puts "La suma de los precios capturados en USD es: #{@usd}"
+  puts "La suma de los precios capturados en CLP es: #{@clp}"
+end
+
+# Initialized the routine
+def routine_1(attributes)
+  puts "#{'-'*74}\n|#{' '*15}Bienvenido al identificador de precios 3b's#{' ' * 15}|\n#{'-' * 74}"
+  puts "\n"*3
+
+  puts "¿Cuantos productos quieres ingresar?"
+  q_products = gets.chomp.to_i
+
+  products = []
+  (1..q_products).each do |iter|
+    puts "#{'-' * 15} Ingresa dato de producto #{iter + 1} #{'-' * 15}"
+    products << new_product(attributes, iter)
+    puts ''
+  end
+
+  products.each_with_index do |product, index|
+    print_product(product, index + 1, attributes)
+  end
+
+  totals
+end
+
+routine_1(@attributes)
---
+++ identificador_3b/productInfo.rb
@@ -0,0 +1,62 @@
+require 'byebug'
+# ========== Products Definition ========================
+
+product1 = ['', 'Palta', 'Fruta', 3900, 0, 'Abarrotes']
+product2 = ['', 'Frutilla', 'Fruta', 1500, 10, 'Abarrotes']
+product3 = ['', 'Jabón', 'Barra', 1000, 20, 'Aseo']
+
+array_products = [product1, product2, product3]
+
+# =======================================================
+
+# Defines an ID for the product
+def id_prod(array_products)
+  array_products.each_with_index do |product, index|
+    id = "#{index+1}#{product[1][0..2].upcase}3BS"
+    product[0] = id
+    product
+  end
+  array_products
+end
+
+# Defines price with taxes
+def tax_price(array_products)
+  array_products.map do |product|
+    iva_price = product[3]*1.19
+    product.append(iva_price)
+  end
+  array_products
+end
+
+# Defines the total to paid with taxes of an array of products
+def total_check(array_products)
+  total = 0
+  array_products.each do |product|
+    total += product[6].to_i
+  end
+  total
+end
+
+# Puts info of products in console
+def prints_totals(array_products)
+  puts "#{'-' * 15} Catalogo de productos #{'-' * 15}\n\n"
+
+  array_products.each do |product|
+    puts "\nLos datos del producto son: \n"
+    puts "Clave: #{product[0]}, Nombre: #{product[1]}"
+    puts "El precio final es #{product[6]}"
+  end
+end
+
+# Runs the routine in order to define the information needed
+def routine_2(array_products)
+  array_products = id_prod(array_products)
+
+  array_products = tax_price(array_products)
+
+  prints_totals(array_products)
+
+  puts "\n\nEl total de la cuenta es: #{total_check(array_products)}"
+end
+
+routine_2(array_products)
