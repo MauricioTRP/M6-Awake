7714c9972bd5e7211db04e9c315b2e0531492c46
Mauricio Fuentes <mauriciofb@gmail.com>
2022-10-18 12:30:46 -0300

    Ejercicio 5

--- identificador_3b/init.rb
+++ identificador_3b/init.rb
@@ -15,24 +15,30 @@ def new_product(iter = '')
     'categoria' => '',
     'precio_final' => 0
   }
-  fill_product_with(product, iter)
+  product = fill_product_with(product, iter)
+  new_register(product)
+  print_a_product(product) if iter == ''
+  print_a_product(product, iter) if iter != ''
 end
 
 # Logic of the product creation
+
+# REFACTOR (CLAVE)
 def fill_product_with(product, iter)
-  product.each_key do |attribute|
+  attributes_to_fill = %w[nombre unidad precio promocion categoria]
+
+  attributes_to_fill.each do |attribute|
     puts "Ingrese #{attribute} del producto #{iter}"
     pass_test = attribute == 'precio' || attribute == 'promocion'
     if pass_test
       product[attribute] << ask_currency if attribute == 'precio'
       product[attribute] << gets.chomp.to_i if attribute == 'promocion'
-    elsif attribute == 'precio_final'
-      product[attribute] = product['precio'].split(' ').first.to_i*1.19
     else
       product[attribute] << gets.chomp
     end
   end
-  new_register(product)
+  product['clave'] = key_of_product(product, products_from_text.length)
+  product['precio_final'] = product['precio'].split(' ').first.to_i*1.19
   product
 end
 
@@ -56,14 +62,10 @@ def ask_currency
   end
 end
 
-# Prints product in console
-def print_product(product_array)
-  product_array.each_with_index do |product, index|
-    puts "#{'-' * 10} La información del producto #{index + 1} es #{'-' * 15}"
-
-    product.each do |key, value|
-      puts "#{key}: #{value}"
-    end
+def print_a_product(product, index = '')
+  puts "#{'-' * 10} La información del producto #{index.to_i} es #{'-' * 15}"
+  product.each do |key, value|
+    puts "#{key}: #{value}"
   end
 end
 
@@ -76,17 +78,16 @@ end
 
 # Initialized the routine for hand input new product
 def new_products_by_hand
-  puts "¿Cuantos productos quieres ingresar?"
+  puts '¿Cuantos productos quieres ingresar?'
   q_products = gets.chomp.to_i
 
   products = []
   (1..q_products).each do |iter|
-    puts "#{'-' * 15} Ingresa dato de producto #{iter + 1} #{'-' * 15}"
-    products << new_product(iter)
+    puts "#{'-' * 15} Ingresa dato de producto #{iter} #{'-' * 15}"
+    products << new_product(iter) if q_products > 1
+    products << new_product if q_products == 1
     puts ''
   end
-
-  print_product(products)
 
   totals
 end
@@ -105,23 +106,22 @@ def products_from_text
 end
 
 def hash_filler(product)
-  # needs an array of products as csv
+  # needs an array of products with each product as csv
   keys = %W[clave nombre unidad precio promocion categoria]
 
-  product_hash = {
-    'clave' => '',
-    'nombre' => '',
-    'unidad' => '',
-    'precio' => '',
-    'promocion' => 0,
-    'categoria' => '',
-    'precio_final' => 0
+  product_hash = { 
+    'clave' => '', 'nombre' => '', 'unidad' => '', 'precio' => '', 'promocion' => 0, 'categoria' => '', 'precio_final' => 0
   }
   product.split(',').each_with_index do |value, index|
     product_hash[keys[index]] = value
+    product_hash['clave'] = "#{index + 1}#{product[1][0..2].upcase}3BS"
   end
-  product_hash["precio_final"] = product_hash["precio"].split(' ').first.to_i*1.19
+  product_hash['precio_final'] = product_hash['precio'].split(' ').first.to_i * 1.19
   product_hash
+end
+
+def key_of_product(product, length = 1)
+  "#{length.to_i}#{product['nombre'].gsub(/\s*/, '')[0..2].upcase}3BS"
 end
 
 # ======== Writes new product ====================================
@@ -129,14 +129,14 @@ def new_register(product)
   values = "\n"
 
   product.each do |key, value|
-    if key != "precio_final"
+    if key != 'precio_final'
       values << "#{value},"
     end
   end
   File.write("#{__dir__}/db/catalogo_productos.txt", values[0...-1], mode: "a")
 end
 
-def prices_array(product_array)
+def prices_array(_product_array)
   prices = []
 
   products_from_text.each do |product|
@@ -148,10 +148,16 @@ end
 
 # ================== Search Product ================================
 
-def search_product(product_array, search_pattern)
+def search_product_by_name(product_array, search_pattern)
   search_pattern = /\w*#{search_pattern}\w*/i
 
   product_array.select{ |product| product['nombre'][search_pattern]}
+end
+
+def search_product_by_id(product_array, search_pattern)
+  search_pattern = /\w*#{search_pattern}\w*/i
+
+  product_array.select{ |product| product['clave'][search_pattern]}
 end
 
 # ================== Prompt User ===================================
@@ -162,7 +168,8 @@ def promt_user
   puts "#{'-' * 25}Catalogo de Productos#{'-' * 25}#{"\n" * 4}"
 
   puts "Qué acción quieres realizar:\n"
-  print " 1. Generar un arreglo de producto
+  print "
+  1. Generar un arreglo de producto
   2. Generar un arreglo de precios
   3. Generar un arreglo de precios con impuestos
   4. Ordenar el arreglo de precios
@@ -170,14 +177,14 @@ def promt_user
   6. Imprimir un productos ordenados por precio
   7. Leer archivos de txt
   8. Agregar un registro de txt
-  9. Imprimir un arreglo ordenado\n\n
+  9. Buscar producto por id\n\n
   Que acción quieres realizar: Salir (q)"
 
   answer = gets.chomp
   while answer != 'q'
     case answer
     when '1'
-      new_product
+      new_products_by_hand
       puts "Producto creado\nQué acción quieres realizar: Salir (q)"
       answer = gets.chomp
     when '2'
@@ -201,7 +208,7 @@ def promt_user
     when '5'
       puts "¿Qué producto deseas buscar?"
       search_pattern = gets.chomp
-      print search_product(products_from_text, search_pattern)
+      print search_product_by_name(products_from_text, search_pattern)
 
       puts "\nQué acción quieres realizar: Salir (q)"
       answer = gets.chomp
@@ -222,8 +229,9 @@ def promt_user
       puts "\nQué acción quieres realizar: Salir (q)"
       answer = gets.chomp
     when '9'
-      array = products_from_text.sort_by{ |product| product['precio_final'] }
-      puts array
+      puts "¿Qué clave de producto deseas buscar?"
+      search_pattern = gets.chomp
+      print search_product_by_id(products_from_text, search_pattern)
 
       puts "\nQué acción quieres realizar: Salir (q)"
       answer = gets.chomp
